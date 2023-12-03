9ccd564cef5e2fde9fac78fa33aa3e31a83bdef3
Fr13nzzz <basti.lutz@online.de>
2023-12-03 20:52:09 +0100

    [BUGFIX] Fix headers

--- src/DriveItemClient.php
+++ src/DriveItemClient.php
@@ -40,7 +40,7 @@ class DriveItemClient extends ResourceClient
 
     public function create(string $driveId, string $parentId, array $jsonPayload, array $options = [], array $queryParameters = []): array
     {
-        $options = array_merge_recursive(['Headers' => ['Content-Type' => 'application/json']], $options);
+        $options = array_merge_recursive(['headers' => ['Content-Type' => 'application/json']], $options);
         $options = array_merge_recursive(['body' => json_encode($jsonPayload)], $options);
 
         return $this->postByResourcePath(
